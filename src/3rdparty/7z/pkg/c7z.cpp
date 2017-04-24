357247b9162e738935248e6df79f951b1ff36cbf
yangsong <yangsongfwd@163.com>
2017-04-25 00:00:46 +0800

    compress

--- src/3rdparty/7z/pkg/c7z.cpp
+++ src/3rdparty/7z/pkg/c7z.cpp
@@ -1,8 +1,25 @@
 
 #include "StdAfx.h"
-
 #include "c7z.h"
 
+#include <CPP/7zip/Common/FileStreams.h>
+#include <CPP/7zip/Archive/IArchive.h>
+#include <CPP/Windows/PropVariant.h>
+#include <CPP/Windows/FileFind.h>
+#include <CPP/Common/IntToString.h>
+#include <CPP/Common/MyCom.h>
+
+#define IS_SPT(x) ((x) == L'/' || (x) == L'\\')
+
+STDAPI CreateObject(const GUID *clsid, const GUID *iid, void **outObject);
+
+DEFINE_GUID(CLSID_CFormat7z,
+	0x23170F69, 0x40C1, 0x278A, 0x10, 0x00, 0x00, 0x01, 0x10, 0x07, 0x00, 0x00);
+DEFINE_GUID(CLSID_CFormatXz,
+	0x23170F69, 0x40C1, 0x278A, 0x10, 0x00, 0x00, 0x01, 0x10, 0x0C, 0x00, 0x00);
+
+#define CLSID_Format CLSID_CFormat7z
+// #define CLSID_Format CLSID_CFormatXz
 
 C7zCompresser::C7zCompresser()
 {
@@ -14,9 +31,47 @@ C7zCompresser::~C7zCompresser()
 
 }
 
-STDMETHODIMP C7zCompresser::AddFile(LPCWSTR szPath, LPCWSTR szInnerPath)
+STDMETHODIMP C7zCompresser::AddFile(LPCWSTR szLocalPath, LPCWSTR szInnerPath)
 {
-	return E_NOTIMPL;
+	FString localPath = us2fs(szLocalPath);
+
+	NWindows::NFile::NFind::CFileInfo fi;
+	if (!fi.Find(localPath) || fi.IsDir())
+		return E_FAIL;
+
+	UString fileName = szLocalPath;
+	int len = wcslen(szLocalPath);
+	int i = len - 1;
+	for (; i >= 0 && !IS_SPT(szLocalPath[i]); --i);
+	if (i >= 0)
+	{
+		UString tmp = szLocalPath;
+		fileName = tmp.RightPtr(len - i - 1);
+	}
+
+	UString innerPath;
+	if (szInnerPath == nullptr)
+	{
+		innerPath = fileName;
+	}
+	else
+	{
+		innerPath = szInnerPath;
+		int len = wcslen(szInnerPath);
+		if (IS_SPT(szInnerPath[len - 1]))
+			innerPath += fileName;
+	}
+
+	CDirItem di;
+	di.Attrib = fi.Attrib;
+	di.Size = fi.Size;
+	di.CTime = fi.CTime;
+	di.ATime = fi.ATime;
+	di.MTime = fi.MTime;
+	di.Name = innerPath;
+	di.LocalPath = localPath;
+	m_files.Add(di);
+	return S_OK;
 }
 
 STDMETHODIMP C7zCompresser::Folder(LPCWSTR szPath)
@@ -24,9 +79,115 @@ STDMETHODIMP C7zCompresser::Folder(LPCWSTR szPath)
 	return E_NOTIMPL;
 }
 
+STDMETHODIMP C7zCompresser::SetTotal(UInt64 total)
+{
+	return S_OK;
+}
+
+STDMETHODIMP C7zCompresser::SetCompleted(const UInt64 *completeValue)
+{
+	return S_OK;
+}
+
 STDMETHODIMP C7zCompresser::Compress(LPCWSTR szOutPath)
 {
-	return E_NOTIMPL;
+	CMyComPtr<COutFileStream> outFileStream(new COutFileStream);
+	if (!outFileStream->Create(us2fs(szOutPath), false))
+		return E_FAIL;
+
+	CMyComPtr<IOutArchive> outArchive;
+	if (CreateObject(&CLSID_Format, &IID_IOutArchive, (void **)&outArchive) != S_OK)
+		return E_FAIL;
+
+	return outArchive->UpdateItems(outFileStream, m_files.Size(), this);
+}
+
+STDMETHODIMP C7zCompresser::GetUpdateItemInfo(UInt32 /* index */,
+	Int32 *newData, Int32 *newProperties, UInt32 *indexInArchive)
+{
+	if (newData)
+		*newData = BoolToInt(true);
+	if (newProperties)
+		*newProperties = BoolToInt(true);
+	if (indexInArchive)
+		*indexInArchive = (UInt32)(Int32)-1;
+	return S_OK;
+}
+
+STDMETHODIMP C7zCompresser::GetProperty(UInt32 index, PROPID propID, PROPVARIANT *value)
+{
+	NWindows::NCOM::CPropVariant prop;
+
+	if (propID == kpidIsAnti)
+	{
+		prop = false;
+		prop.Detach(value);
+		return S_OK;
+	}
+
+	{
+		const CDirItem &dirItem = m_files[index];
+		switch (propID)
+		{
+		case kpidPath:  prop = dirItem.Name; break;
+		case kpidIsDir:  prop = dirItem.isDir(); break;
+		case kpidSize:  prop = dirItem.Size; break;
+		case kpidAttrib:  prop = dirItem.Attrib; break;
+		case kpidCTime:  prop = dirItem.CTime; break;
+		case kpidATime:  prop = dirItem.ATime; break;
+		case kpidMTime:  prop = dirItem.MTime; break;
+		}
+	}
+	prop.Detach(value);
+	return S_OK;
+}
+
+
+STDMETHODIMP C7zCompresser::GetStream(UInt32 index, ISequentialInStream **inStream)
+{
+	const CDirItem &dirItem = m_files[index];
+
+	if (dirItem.isDir())
+		return S_OK;
+
+	{
+		CMyComPtr<CInFileStream> inStreamLoc(new CInFileStream);
+		FString path = dirItem.LocalPath;
+		if (!inStreamLoc->Open(path))
+		{
+			DWORD sysError = ::GetLastError();
+			return E_FAIL;
+		}
+
+		*inStream = inStreamLoc.Detach();
+	}
+	return S_OK;
+}
+
+STDMETHODIMP C7zCompresser::SetOperationResult(Int32 operationResult)
+{
+	return S_OK;
+}
+
+STDMETHODIMP C7zCompresser::GetVolumeSize(UInt32 index, UInt64 *size)
+{
+	return S_FALSE;
+}
+
+STDMETHODIMP C7zCompresser::GetVolumeStream(UInt32 index, ISequentialOutStream **volumeStream)
+{
+	wchar_t temp[16];
+	ConvertUInt32ToString(index + 1, temp);
+	UString res = temp;
+	while (res.Len() < 2)
+		res.InsertAtFront(L'0');
+	UString fileName = L'.';
+	fileName += res;
+	CMyComPtr<COutFileStream> streamLoc(new COutFileStream);
+	if (!streamLoc->Create(us2fs(fileName), false))
+		return ::GetLastError();
+	*volumeStream = streamLoc.Detach();
+	return S_OK;
 }
 
 HRESULT _Create7zCompresser(I7zCompresser** compresser)
--- src/3rdparty/7z/pkg/c7z.h
+++ src/3rdparty/7z/pkg/c7z.h
@@ -3,10 +3,26 @@
 #define __7Z_C7Z_H__
 
 #include "../7z.h"
+#include <CPP/7zip/Archive/IArchive.h>
+#include <vector>
+#include <CPP/Common/MyString.h>
 
+struct CDirItem
+{
+	UInt64 Size;
+	FILETIME CTime;
+	FILETIME ATime;
+	FILETIME MTime;
+	UString Name;
+	FString LocalPath;
+	UInt32 Attrib;
+
+	bool isDir() const { return (Attrib & FILE_ATTRIBUTE_DIRECTORY) != 0; }
+};
 
 class C7zCompresser
 	: public I7zCompresser
+	, public IArchiveUpdateCallback2
 	, private CMyUnknownImp
 {
 public:
@@ -15,9 +31,14 @@ public:
 	C7zCompresser();
 	~C7zCompresser();
 
+	INTERFACE_IArchiveUpdateCallback2()
+
 	STDMETHOD(AddFile) (LPCWSTR szPath, LPCWSTR szInnerPath) override;
 	STDMETHOD(Folder) (LPCWSTR szPath) override;
 	STDMETHOD(Compress) (LPCWSTR szOutPath) override;
+
+private:
+	CObjectVector<CDirItem> m_files;
 };
 
 
